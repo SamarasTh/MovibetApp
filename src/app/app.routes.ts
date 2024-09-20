6d14cc173233cf5a40bb10b098cddf57842b1c89
Thanos <ths1311@hotmail.com>
2024-09-20 19:56:03 +0300

    Routes / adds basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: (urlInfo) => {
+            console.log(urlInfo);
+            return '/home'
+        },
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: SearchComponent,
+    }
+];
