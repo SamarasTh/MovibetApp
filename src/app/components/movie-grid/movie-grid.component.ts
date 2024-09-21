ffb207e3d2e9861b58af226d2d69e7d321a57211
Thanos <ths1311@hotmail.com>
2024-09-21 19:31:25 +0300

    Movie Grid / add mat paginator

--- src/app/components/movie-grid/movie-grid.component.ts
+++ src/app/components/movie-grid/movie-grid.component.ts
@@ -2,6 +2,9 @@ import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MovieService } from '../../service/movie.service';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Movie, MovieApiResponse } from '../../model/movie.model';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-movie-grid',
@@ -9,19 +12,38 @@ import { MovieService } from '../../service/movie.service';
   imports: [
     CommonModule,
     MatCardModule,
+    MatPaginatorModule
   ]
   ,
   templateUrl: './movie-grid.component.html',
   styleUrl: './movie-grid.component.scss'
 })
-export class MovieGridComponent implements OnInit {
-  @Input() movies: any[] = [];
+export class MovieGridComponent implements OnInit, OnDestroy {
+  @Input() moviesApiResponse?: MovieApiResponse;
+  @Input() searchQuery: string = '';
+  movies: Movie[] = [];
   imgUrl?: string;
+  currentPage: number = 1;
+  totalResults: number = 0;
 
+  private destroy$ = new Subject<void>();
 
   constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+
+    this.assignResponseData();
+    console.log('moviesApiResponse ONCHANGES', this.moviesApiResponse);
+  }
+
+  assignResponseData() {
+    if (!this.moviesApiResponse) return;
+    this.movies = this.moviesApiResponse.results;
+    this.totalResults = this.moviesApiResponse.total_results;
   }
 
   getImageUrl(posterPath: string) {
@@ -29,6 +51,30 @@ export class MovieGridComponent implements OnInit {
   }
 
   toggleMovieDetails(movieId: number): void {
+  }
+
+  fetchNextPage() {
+    this.movieService.searchMovies(this.searchQuery, this.currentPage)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe(
+        (response: MovieApiResponse) => {
+          this.moviesApiResponse = response;
+          this.assignResponseData();
+        }
+
+      );
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.fetchNextPage();
+    this.currentPage = event.pageIndex + 1;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(); 
+    this.destroy$.complete(); 
   }
 }
 
