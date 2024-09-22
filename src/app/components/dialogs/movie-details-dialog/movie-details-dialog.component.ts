dc4f5b7b7c308e8066d58fe073e655dfe90ebbc0
Thanos <ths1311@hotmail.com>
2024-09-22 11:34:54 +0300

    Movie Details Dialog / creates component

---
+++ src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-details-dialog.component.html',
+  styleUrl: './movie-details-dialog.component.scss'
+})
+export class MovieDetailsDialogComponent {
+
+}
