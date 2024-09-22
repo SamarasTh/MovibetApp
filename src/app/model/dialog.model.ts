bf93a68505135ee1f05163daeb786edd8bfb7e72
Thanos <ths1311@hotmail.com>
2024-09-22 19:05:04 +0300

    Dialog / Model / create model for dialog data and config  for responsive styling

---
+++ src/app/model/dialog.model.ts
@@ -0,0 +1,25 @@
+import { MatDialogConfig } from "@angular/material/dialog";
+
+
+export const mvbtDesktopDialogConfig = {
+    panelClass: ['mvbt-dialog', 'animate__animated', ' animate_slideInUp', 'animate_faster'],
+    maxWidth: '100%',
+} as MatDialogConfig;
+
+export const mvbtMobileDialogConfig = {
+    panelClass: ['mvbt-dialog', 'animate__animated', ' animate_slideInUp', 'animate_faster'],
+    maxWidth: '100%',
+} as MatDialogConfig;
+
+
+export interface dialogData {
+    title: string,
+    overview: string,
+    poster_path: string,
+    budget: number,
+    release_date: string,
+    revenue: number,
+    vote_average: number,
+    vote_count: number,
+    spoken_languages: string[]
+}
