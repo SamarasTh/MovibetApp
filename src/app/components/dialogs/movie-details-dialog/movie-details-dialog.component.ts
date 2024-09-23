5dd2a96843ecadc27e08959735ab94d8c596d6d3
Thanos <ths1311@hotmail.com>
2024-09-23 09:54:48 +0300

    Movie Details Dialog / creates movie details modal which  user can toggle

--- src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
+++ src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { KeepOneDigitPipe } from '../../pipe/keepOneDigit.pipe';
+import { MovieService } from '../../../service/movie.service';
+import { DialogData } from '../../../model/dialog.model';
 
 @Component({
   selector: 'app-movie-details-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    KeepOneDigitPipe
+  ],
   templateUrl: './movie-details-dialog.component.html',
   styleUrl: './movie-details-dialog.component.scss'
 })
 export class MovieDetailsDialogComponent {
 
+
+  constructor(
+    public dialogRef: MatDialogRef<MovieDetailsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public movie: DialogData,
+    private movieService: MovieService,
+  ) {
+
+  }
+
+  getImageUrl(posterPath: string) {
+    return this.movieService.getImageUrl(posterPath);
+  }
 }
