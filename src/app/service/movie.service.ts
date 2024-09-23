874c42520606a0bfb6dd1f29d7bf15c4d9961848
Thanos <ths1311@hotmail.com>
2024-09-23 09:24:33 +0300

    Movie Service / adds get movie by Id

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import { environment } from '../../environment/environment';
 import { MovieApiResponse } from '../model/movie.model';
+import { DialogData } from '../model/dialog.model';
 
 @Injectable({
     providedIn: 'root'
@@ -21,5 +22,10 @@ export class MovieService {
     getImageUrl(posterPath: string): string {
         return `https://image.tmdb.org/t/p/w500${posterPath}`;
     }
+
+    getMovie(movieId: number): Observable<DialogData> {
+        return this.http.get<DialogData>(`${this.apiUrl}/movie/${movieId}?api_key=${this.apiKey}`);
+    }
+
 
 }
