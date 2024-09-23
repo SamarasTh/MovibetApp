c0475ec5b8e645e8f2b5f319a5643c29d075259e
Thanos <ths1311@hotmail.com>
2024-09-23 09:26:25 +0300

    Dialog Service / adds openMovieDetailsDialog

--- src/app/service/dialog.service.ts
+++ src/app/service/dialog.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { MatDialog } from '@angular/material/dialog';
 import { ScrollStrategyOptions } from '@angular/cdk/overlay';
+import { MovieService } from './movie.service';
+import { mvbtDesktopDialogConfig } from '../model/dialog.model';
 
 @Injectable({
     providedIn: 'root'
@@ -11,10 +13,24 @@ export class DialogService {
 
     constructor(
         public dialog: MatDialog,
-        private scrollStrategyOptions: ScrollStrategyOptions
+        private scrollStrategyOptions: ScrollStrategyOptions,
+        private movieService : MovieService
     ) { }
 
 
+    async openMovieDetailsDialog(movieId: number){
+        const {MovieDetailsDialogComponent} = await import(
+            '../components/dialogs/movie-details-dialog/movie-details-dialog.component'
+        );
 
+        this.movieService.getMovie(movieId).subscribe((data) => {
+            console.log(data, 'MOVIE DATA ON CLICK')
+            this.dialog.open(MovieDetailsDialogComponent, {
+                ...mvbtDesktopDialogConfig,
+                scrollStrategy: this.scrollStrategyOptions.noop(),
+                data
+            })
+        })
+    }
 
 }
