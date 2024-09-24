696e13985bd662faaaf56ea28f4c94c371f2d245
Thanos <ths1311@hotmail.com>
2024-09-24 08:42:22 +0300

    Movie Details / adds rating functionality - todo make it better

--- src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
+++ src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
@@ -1,32 +1,41 @@
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 import { KeepOneDigitPipe } from '../../../pipe/keepOneDigit.pipe';
 import { MovieService } from '../../../service/movie.service';
 import { DialogData } from '../../../model/dialog.model';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-details-dialog',
   standalone: true,
   imports: [
+    FormsModule,
     CommonModule,
     MatIconModule,
     MatDialogModule,
-    KeepOneDigitPipe
+    KeepOneDigitPipe,
   ],
   templateUrl: './movie-details-dialog.component.html',
   styleUrl: './movie-details-dialog.component.scss'
 })
-export class MovieDetailsDialogComponent {
+export class MovieDetailsDialogComponent implements OnInit {
+  guestSessionId: string | null = null;
+  rating: number = 0;
 
+  private destroy$ = new Subject<void>();
 
   constructor(
     public dialogRef: MatDialogRef<MovieDetailsDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public movie: DialogData,
     private movieService: MovieService,
-  ) {
+  ) { }
+
+  ngOnInit() {
+    this.initGuestSession();
 
   }
 
@@ -36,5 +45,28 @@ export class MovieDetailsDialogComponent {
 
   onPrimaryClick() {
     this.dialogRef.close();
+  }
+
+  initGuestSession() {
+    this.movieService.generateGuestSession().subscribe(response => {
+      this.guestSessionId = response.guest_session_id;
+    });
+  }
+
+  postRating() {
+    if (this.guestSessionId && this.rating > 0 && this.movie.id) {
+      this.movieService.rateMovie(this.movie.id, this.rating, this.guestSessionId)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(response => {
+          alert('Rating submitted successfully!');
+        });
+    } else {
+      alert('Movie ID or guest session is missing');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
