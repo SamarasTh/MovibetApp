911550f5fbacfd1fdc40613ccfe9e98078f4ba69
Thanos <ths1311@hotmail.com>
2024-09-24 21:32:15 +0300

    Movie Details Dialog / pretify rating slider

--- src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
+++ src/app/components/dialogs/movie-details-dialog/movie-details-dialog.component.ts
@@ -24,6 +24,7 @@ import { DialogData } from '../../../model/dialog.model';
 })
 export class MovieDetailsDialogComponent implements OnInit {
   guestSessionId: string | null = null;
+  ratingNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   rating: number = 0;
 
   private destroy$ = new Subject<void>();
