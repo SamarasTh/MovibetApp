c79d49eec8f621d064f14bf768d4d5801854d6b4
Thanos <ths1311@hotmail.com>
2024-09-25 20:23:13 +0300

    Dialog Model / remove configs for mobile and desktop not time for this to make it work

--- src/app/model/dialog.model.ts
+++ src/app/model/dialog.model.ts
@@ -1,16 +1,3 @@
-import { MatDialogConfig } from "@angular/material/dialog";
-
-
-export const mvbtDesktopDialogConfig = {
-    panelClass: ['mvbt-dialog'],
-    maxWidth: '100%',
-} as MatDialogConfig;
-
-export const mvbtMobileDialogConfig = {
-    panelClass: ['mvbt-dialog', 'animate__animated', ' animate_slideInUp', 'animate_faster'],
-    maxWidth: '100%',
-} as MatDialogConfig;
-
 
 export interface DialogData {
     title: string,
