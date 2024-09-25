cb0e0ec758168a84c2091507f281d42d58fbc007
Thanos <ths1311@hotmail.com>
2024-09-25 20:26:35 +0300

    Collections Dialog / add basic  structure and functionality of collection dialog

--- src/app/components/dialogs/collections-dialog/collections-dialog.component.ts
+++ src/app/components/dialogs/collections-dialog/collections-dialog.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-collections-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+  ],
   templateUrl: './collections-dialog.component.html',
   styleUrl: './collections-dialog.component.scss'
 })
 export class CollectionsDialogComponent {
+  collections: any[] = [];
+  selectedCollection: any;
 
+  constructor(
+    public dialogRef: MatDialogRef<CollectionsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  selectCollection(collection: any): void {
+
+  }
+  cancelDialog(): void {
+    this.dialogRef.close();
+  }
 }
