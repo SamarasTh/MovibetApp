89cbd4186ed3910cad540fdb4bb1f33c5dc3bef6
Thanos <ths1311@hotmail.com>
2024-09-26 09:30:58 +0300

    Collections page / work in progress

--- src/app/components/collections-page/collections-page.component.ts
+++ src/app/components/collections-page/collections-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-collections-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './collections-page.component.html',
   styleUrl: './collections-page.component.scss'
 })
