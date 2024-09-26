87357e3f34665444f23620a2708fd581a63b8e10
Thanos <ths1311@hotmail.com>
2024-09-26 21:19:16 +0300

    App.Routes / add more paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
 import { CollectionsPageComponent } from './components/collections-page/collections-page.component';
+import { CollectionCreatePageComponent } from './components/collection-create-page/collection-create-page.component';
+import { CollectionDetailsPageComponent } from './components/collection-details-page/collection-details-page.component';
 
 export const routes: Routes = [
     {
@@ -15,8 +17,17 @@ export const routes: Routes = [
         path: 'search',
         component: SearchComponent,
     },
-    { 
+    {
         path: 'collections',
-        component: CollectionsPageComponent },
+        component: CollectionsPageComponent
+    },
+    {
+        path: 'collections/create',
+        component: CollectionCreatePageComponent
+    },
+    {
+        path: 'collections/:title',
+        component: CollectionDetailsPageComponent
+    },
 
 ];
