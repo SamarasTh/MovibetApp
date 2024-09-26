cb75006befb119090c9bc95453e2765e3021b097
Thanos <ths1311@hotmail.com>
2024-09-26 10:40:17 +0300

    Collections pAge / work in progress

--- src/app/components/collections-page/collections-page.component.ts
+++ src/app/components/collections-page/collections-page.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-collections-page',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './collections-page.component.html',
   styleUrl: './collections-page.component.scss'
 })
-export class CollectionsPageComponent {
+export class CollectionsPageComponent implements OnInit {
   collections: any[] = [];
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit(): void {
     this.loadCollections();
   }
 
   loadCollections() {
+  }
+
+  goHome(){
+    this.router.navigate(['/search']);
   }
 }
