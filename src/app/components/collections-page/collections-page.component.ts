559212b7591f4384aae45e2ce406392776928fa9
Thanos <ths1311@hotmail.com>
2024-09-26 21:23:46 +0300

    Collections Page / load  and populates collections and navigates to collection clicked

--- src/app/components/collections-page/collections-page.component.ts
+++ src/app/components/collections-page/collections-page.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-collections-page',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './collections-page.component.html',
   styleUrl: './collections-page.component.scss'
 })
@@ -21,7 +22,23 @@ export class CollectionsPageComponent implements OnInit {
   }
 
   loadCollections() {
+    const storedCollections = localStorage.getItem('movieCollections');
+    if (storedCollections) {
+      this.collections = JSON.parse(storedCollections);
+    } else {
+      this.collections = [];
+    }
   }
+
+  navigateToCreateCollectionPage(){
+    this.router.navigate(['/collections/create']);
+  }
+
+
+  toggleCollection(title : string){
+    this.router.navigate([`/collections/${title}`]);
+  }
+  
 
   goHome(){
     this.router.navigate(['/search']);
